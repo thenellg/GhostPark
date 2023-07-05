f17f65a7092e868a99246aa07551c8517d842f1b
Griffin Thenell <griffin.thenell@serenityforge.com>
2023-07-04 18:01:03 -0700

    Added key rebinding saving

--- Assets/Scripts/playerSettings.cs
+++ Assets/Scripts/playerSettings.cs
@@ -28,14 +28,110 @@ public class playerSettings : MonoBehaviour
     public string dash = "q";
     public string hold = "e";
 
-    public List<string> keyboardDefault = new List<string>();
-    public List<string> playstationDefault = new List<string>();
-    public List<string> xboxDefault = new List<string>();
-    public List<string> switchDefault = new List<string>();
+    public List<string> keyboardControls = new List<string>();
+    public List<string> playstationControls = new List<string>();
+    public List<string> xboxControls = new List<string>();
+    public List<string> switchControls = new List<string>();
 
-    // Start is called before the first frame update
-    void Start()
+    public bool active = false;
+
+    private void Update()
     {
+        if (active)
+        {
+            var controllers = Input.GetJoystickNames();
+            foreach (string controller in controllers)
+                Debug.Log(controller);
+            active = false;
+        }
+    }
 
+    public void changeSettings()
+    {
+        if (currentController == controlType.keyboard)
+        {
+            up = keyboardControls[0];
+            down = keyboardControls[1];
+            left = keyboardControls[2];
+            right = keyboardControls[3];
+            jump = keyboardControls[4];
+            dash = keyboardControls[5];
+            hold = keyboardControls[6];
+}
+        if (currentController == controlType.Playstation)
+        {
+            up = playstationControls[0];
+            down = playstationControls[1];
+            left = playstationControls[2];
+            right = playstationControls[3];
+            jump = playstationControls[4];
+            dash = playstationControls[5];
+            hold = playstationControls[6];
+        }
+        else if (currentController == controlType.Xbox)
+        {
+            up = xboxControls[0];
+            down = xboxControls[1];
+            left = xboxControls[2];
+            right = xboxControls[3];
+            jump = xboxControls[4];
+            dash = xboxControls[5];
+            hold = xboxControls[6];
+        }
+        else if (currentController == controlType.Switch)
+        {
+            up = switchControls[0];
+            down = switchControls[1];
+            left = switchControls[2];
+            right = switchControls[3];
+            jump = switchControls[4];
+            dash = switchControls[5];
+            hold = switchControls[6];
+        }
+    }
+
+    public void resetDefault()
+    {
+        if (currentController == controlType.keyboard)
+        {
+            keyboardControls[0] = "w";
+            keyboardControls[1] = "s";
+            keyboardControls[2] = "a";
+            keyboardControls[3] = "d";
+            keyboardControls[4] = "space";
+            keyboardControls[5] = "q";
+            keyboardControls[6] = "e";
+        }
+        if (currentController == controlType.Playstation)
+        {
+            playstationControls[0] = "w";
+            playstationControls[1] = "s";
+            playstationControls[2] = "a";
+            playstationControls[3] = "d";
+            playstationControls[4] = "space";
+            playstationControls[5] = "q";
+            playstationControls[6] = "e";
+        }
+        else if (currentController == controlType.Xbox)
+        {
+            xboxControls[0] = "w";
+            xboxControls[1] = "s";
+            xboxControls[2] = "a";
+            xboxControls[3] = "d";
+            xboxControls[4] = "space";
+            xboxControls[5] = "q";
+            xboxControls[6] = "e";
+        }
+        else if (currentController == controlType.Switch)
+        {
+            switchControls[0] = "w";
+            switchControls[1] = "s";
+            switchControls[2] = "a";
+            switchControls[3] = "d";
+            switchControls[4] = "space";
+            switchControls[5] = "q";
+            switchControls[6] = "e";
+        }
+        changeSettings();
     }
 }
