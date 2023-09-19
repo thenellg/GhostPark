2eea1fd5b7f527f95786be20ee6bb3e00359ff8b
Griffin Thenell <32246958+thenellg@users.noreply.github.com>
2023-09-19 13:11:05 -0700

    Added new projectile

---
+++ Assets/Scripts/Obstacles/projectilePlayerOnly.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectilePlayerOnly : MonoBehaviour
+{
+    public float speed;
+    public bool directionLeft;
+    public Rigidbody2D m_Rigidbody2D;
+    public Transform parent;
+
+    public float timer = 0f;
+    public float maxTime = 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        transform.parent = GameObject.Find("Spawning Hazards").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Rigidbody2D.AddForce(new Vector2(-speed, 0));
+
+        if(timer < maxTime)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController temp = FindObjectOfType<PlayerController>();
+        if (collision.tag == "Player" || collision.tag == "Minecart")
+        {
+            if(collision.transform.position.y > transform.position.y)
+            {
+                temp.controller.m_Rigidbody2D.AddForce(new Vector2(0, temp.controller.m_JumpForce));
+            }
+            else
+            {
+                if (!temp.controller.m_Settings.invincibility)
+                    temp.onDeath();
+                else
+                    Destroy(this.gameObject);
+            }
+        }
+    }
+
+
+}
